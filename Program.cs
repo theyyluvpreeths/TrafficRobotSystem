5d8956e6eed2ed4e40ece9da03231432d11a1715
ST10447872 <keshavadityapreethlal@gmail.com>
2026-02-18 15:11:17 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+class TrafficRobot
+{
+    static void Main()
+    {
+        while (true)
+        {
+            GreenLight();
+            YellowLight();
+            RedLight();
+        }
+    }
+
+    static void GreenLight()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("🚦 TRAFFIC LIGHT");
+        Console.WriteLine("GREEN LIGHT - GO");
+        Countdown(10);
+    }
+
+    static void YellowLight()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("🚦 TRAFFIC LIGHT");
+        Console.WriteLine("YELLOW LIGHT - SLOW DOWN");
+        Countdown(3);
+    }
+
+    static void RedLight()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("🚦 TRAFFIC LIGHT");
+        Console.WriteLine("RED LIGHT - STOP");
+        Countdown(7);
+    }
+
+    static void Countdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.WriteLine("Time remaining: " + i + " seconds");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrafficRobotSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("3Shape TRIOS A/S")]
+[assembly: AssemblyProduct("TrafficRobotSystem")]
+[assembly: AssemblyCopyright("Copyright © 3Shape TRIOS A/S 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8106ed1c-1564-4af7-8f34-43bbfe957dd5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
